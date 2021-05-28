7762aeac764951a77e62bb4da8e2e211c8fe1a01
Sameeksha Gupta <54001595+Nyble23@users.noreply.github.com>
2021-05-28 16:43:36 +0530

    Update corresponding_cue.py

--- Co-Saliency Detection/corresponding_cue.py
+++ Co-Saliency Detection/corresponding_cue.py
@@ -1,1 +1,15 @@
 
+# This function computes corresponding cue of each colour cluster that is identified in the input image,
+# and returns a dictionary, containing contrast cue value of each cluster.
+
+def calculate_corresponding_cue():
+  corresponding_cue = defaultdict(list)
+
+  for k in range(clusters):
+    nk = Counter(label)[k]           # number of pixels in kth cluster
+
+    for m in range(len(image_list)):
+      clus_label = label_list[m]
+      corresponding_cue[k].append((Counter(clus_label)[k]/nk))
+      
+  return corresponding_cue
